629f4c0566538afb417912aac405ef6c57b58d21
Damien <legat@CQJ>
2024-01-09 10:50:20 +0100

    damien css

--- script/delete.js
+++ script/delete.js
@@ -1,14 +1,14 @@
 
-    export function functionDeleteTask(event) {
-        const taskElement = event.target.parentElement;
-        const taskId = taskElement.id;
+export function functionDeleteTask(event) {
+    const taskElement = event.target.parentElement;
+    const taskId = taskElement.id;
 
-        taskElement.remove();
+    taskElement.remove();
 
-        let tasks = JSON.parse(localStorage.getItem('Tableau'));
-        tasks = tasks.filter(task => task.id !== taskId);
-        localStorage.setItem('Tableau', JSON.stringify(tasks));
-    }
+    let tasks = JSON.parse(localStorage.getItem('Tableau'));
+    tasks = tasks.filter(task => task.id !== taskId);
+    localStorage.setItem('Tableau', JSON.stringify(tasks));
+}
 
 
       
